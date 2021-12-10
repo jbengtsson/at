a52f9264f7b97e08737a2e70a7eaa8408dcb91d1
J.B <jbngtssn@gmail.com>
2021-12-10 16:08:54 +0100

    Code "untweak".

--- atintegrators/tracy-2.cc
+++ atintegrators/tracy-2.cc
@@ -135,6 +135,11 @@ static double* atGetOptionalDoubleArray(const PyObject *element, char *name)
   return atGetOptionalDoubleArraySz(element, name, &msz, &nsz);
 }
 
+
+extern "C" struct elem_type*
+trackFunction(const PyObject *ElemData, struct elem_type *Elem, double ps[],
+	      const int num_particles, const struct parameters *Param);
+
 //------------------------------------------------------------------------------
 
 // Interface functions for: Armidillo vectors, STL vectors, and C arrays.
