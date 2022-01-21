c98c15e310896d773d1f805299d2ef2b83fa78ce
J.B <jbngtssn@gmail.com>
2022-01-21 21:15:50 +0100

    Tweaked pyat_jb.py.

--- pyat/pyat_jb.py
+++ pyat/pyat_jb.py
@@ -217,7 +217,7 @@ lat_names = {
     'bessy-iii' : lat_dir+'/BESSY-III/NoTG-TGRB-B60-6bend-6sx_JB_tracy.lat'
 }
 
-lat_name = 'dba'
+lat_name = 'bessy-iii'
 # lat_name = 'hmba'
 # lat_name = 'bessy-iii'
 if lat_name != 'bessy-iii':
@@ -234,7 +234,7 @@ if False:
 
 lat.radiation_off()
 
-if not False:
+if False:
     tst_cases(lat)
     exit(0)
 
